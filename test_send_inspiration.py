e615f2d58f45f45d5e1507116478315880f6a7a5
katlegoJKS <katlego.malatjie@umuzi.org>
2020-07-29 17:50:31 +0200

    updated

--- test_send_inspiration.py
+++ test_send_inspiration.py
@@ -6,9 +6,12 @@ from send_inspiration import send_email
 
 EMAIL_TO = os.environ.get('EMAIL_TO')
 
-def test_send_email():
-    send_email()
-    # assertEmailSent(to=EMAIL_TO)
-    assertEqual(len(self.get_sent_messages()), 1)
+class TestSendEmail(unittest.TestCase):
+
+    def test_send_email(self):
+        # send_email()
+        # assertEmailSent(to=EMAIL_TO)
+        self.assertEqual(len(self.get_sent_messages()), 1)
    
-    
+if __name__ == '__main__':
+    unittest.main()    
